8e1d7acf826a1bb5cfd06f465c504abf56fe1c77
maheswari06 <dmaheshwaripriya@gmail.com>
2020-09-30 12:45:09 +0530

    Now Updated

---
+++ MavenApp1/src/test/java/com/dev/practice/AppTest.java
@@ -0,0 +1,30 @@
+package com.dev.practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test1() {
+	System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+	System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+	System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+	System.out.println("test4");
+	}
+
+}
